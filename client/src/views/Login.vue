aead5e8be76e7cb17924c61d7d16977f37a83f6b
rafiqii <46050827+rafiqii@users.noreply.github.com>
2020-04-18 09:30:51 +0400

    Login page is made css is done as well, maybe needs a bit of tweaking

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,17 +1,62 @@
 <template>
-  <main>
-    <h1>Test</h1>
+  <main id="Login">
+    <h1>Login Screen</h1>
+  <body>
+    <br>
+    <input v-model="userName" placeholder="enter username" > 
+    <br><br>
+    <input v-model="passWord" placeholder="enter password"> 
+    <br><br>
+    <button @click="LogUserIn">
+      Login
+    </button>
+    <br>
+    
+    
+  </body>
   </main>
 </template>
 
 <script>
   export default {
-    name: 'Login'
+    name: 'Login',
+    data()
+    {
+      return {
+      userName: '',
+      passWord: ''
+      }
+    },
+  methods:
+  {
+    LogUserIn() 
+    {
+      //here we send the info to the server
+      this.userName='',
+      this.passWord=''
+      //here we would check if true or false\
+      //if false borders go red, lets see how it goes
+    }
+
+  }
   }
 </script>
 
 <style scoped>
   h1 {
-    color: brown;
+    color:forestgreen;
+  }
+  input
+  {
+    padding: 15px;
+    border-radius: 7ch;
+    
+  }
+  button {
+    background: #42b983;
+    border: none;
+    padding: 15px;
+    margin: 15px;
+    color: #fff;
   }
 </style>
