c0ab7b02546095aad59a394bd8c82b7eff3f6aa9
just-hussain <1just.hussain@gmail.com>
2020-04-26 01:15:49 +0300

    filter by referee. add new match, post to backend

--- client/src/views/Matches.vue
+++ client/src/views/Matches.vue
@@ -14,6 +14,13 @@
       <h3 slot="header">Add a New Match</h3>
       <div slot="body">
         <!-- input fields for adding new match -->
+        <input v-model="new_match.id" type="text" placeholder="id...">
+        <input v-model="new_match.date" type="text" placeholder="date...">  
+        <input v-model="new_match.time" type="text" placeholder="time...">  
+        <input v-model="new_match.team1" type="text" placeholder="team 1...">  
+        <input v-model="new_match.team2" type="text" placeholder="team 2...">  
+        <input v-model="new_match.field" type="text" placeholder="field...">  
+        <input v-model="new_match.referee" type="text" placeholder="referee...">  
       </div>
     </BaseModal>
 
@@ -25,7 +32,7 @@
     >
       <h3 slot="header">Matches by Referee</h3>
       <div slot="body">
-        <input type="text" placeholder="referee...">        
+        <input v-model="ref_name" type="text" placeholder="referee...">        
       </div>
     </BaseModal>
 
@@ -43,7 +50,8 @@
 <script>
   import BaseMatch from './../components/BaseMatch'
   import BaseModal from './../components/BaseModal'
-  import Home from './Home.vue'
+  import {Match} from './../models/Models'
+  import {api} from './../services/Api'
 
   export default {
     name: 'Matches',
@@ -60,13 +68,18 @@
       return {
         modal_match: false,
         modal_referee: false,
-        matches: Home.data().tour.matches
+        matches: [],
+        ref_name: '',
+        new_match: new Match()
       }
     },
 
     methods: {
       addMatch() {
         console.log(`from add match`);
+
+        api.post(`/matches?id=${this.new_match.id}&date=${this.new_match.date}&time=${this.new_match.time}&team1=${this.new_match.team1}&team2=${this.new_match.team2}&field=${this.new_match.field}&referee=${this.new_match.referee}`)
+
         this.modal_match = false
       },
 
@@ -78,8 +91,18 @@
         // a computed array can be used
         
         console.log(`from by referee`);
+
+        this.matches = this.matches.filter((match) => {
+          return match.referee.toLowerCase() == this.ref_name.toLowerCase()
+        })
+
         this.modal_referee = false
       }
+    },
+
+    created: async function() {
+      this.matches = (await api.get('/matches')).data
+      
     }
   }
 </script>
