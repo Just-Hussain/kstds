bcd0f00b912d4afe3566846afcfdc358c746b6fa
just-hussain <1just.hussain@gmail.com>
2020-04-28 08:51:33 +0300

    base bac-end func for getting players by team and match. a bit of refactoting.

--- client/src/views/Teams.vue
+++ client/src/views/Teams.vue
@@ -6,6 +6,7 @@
     </button>
 
     <BaseModal
+      class="filter-modal"
       v-if="modal_get"
       action="players-by"
       @close="modal_get = false"
@@ -16,16 +17,27 @@
     </h3>
 
     <div slot="body">
-      <input type="text" placeholder="Enter Team...">
-      <input type="text" placeholder="Enter Match...">
+      <input 
+        v-model="query_team" 
+        type="text" 
+        placeholder="Enter Team..."
+      >
+      <input 
+        v-model="query_match"
+        type="text" 
+        placeholder="Enter Match..."
+      >
 
       <BaseTable style="color:#fff">
         <tr>
           <th>Players</th>
         </tr>
 
-        <tr>
-          <td>That guy</td>
+        <tr
+        v-for="player in players"
+        :key="player"
+        >
+          <td>{{player}}</td>
         </tr>
       </BaseTable>
     </div>
@@ -80,13 +92,20 @@
           // new Team(0, 'Team XXX', [], 0, 0, 0, 0),
           // new Team(1, 'Team YYY', [], 0, 0, 0, 0),
           // new Team(2, 'Team ZZZ', [], 0, 0, 0, 0)
-        ]
+        ],
+        query_team: '',
+        query_match: '',
+        players: []
       }
     },
 
     methods: {
-      getPlayers() {
+      async getPlayers() {
         console.log('from get players')
+
+        this.players = (await api.get(`/player?team=${this.query_team}&match=${this.query_match}`)).data
+        
+
       }
     },
 
@@ -122,4 +141,21 @@
     margin: 10px;
     margin-bottom: 25px;
   }
+
+  .filter-modal table tr:nth-child(even) {
+    background-color: #2c3e5081;
+  }
+  .filter-modal table tr:nth-child(odd) {
+    background-color: #14202c81;
+  }
+  .filter-modal table tr:hover {
+    background-color: #42b983e7;
+  }
+  .filter-modal table {
+    display: block;
+    width: max-content;
+    max-height: 470px;
+    overflow-y: scroll;
+  }
+  
 </style>
